d7a9f4afdf285908bb3c1fece3d9d0c90fdca359
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-12-10 15:57:28 -0800

    day 5 - rolled up and smoked

---
+++ 05-alchemical-reduction/ab.js
@@ -0,0 +1,45 @@
+function react(input){
+    input = input.split('');
+    let i = 0;
+    while (i < input.length - 1){
+        const currentUnit = input[i];
+        const unitToTheRight = input[i + 1];
+        if (currentUnit.toUpperCase() === unitToTheRight.toUpperCase() && currentUnit !== unitToTheRight){
+            input.splice(i, 2);
+            if (i > 0){
+                i--;
+            }
+        } else {
+            i++;
+        }
+    }
+    return input;
+}
+
+function removeUnits(unitToDelete, input){
+    input = input.split('');
+    return input.filter(unit => unit !== unitToDelete && unit !== unitToDelete.toUpperCase()).join('');
+}
+
+const fs = require('fs');
+const input =   fs
+                .readFileSync(`${__dirname}/input.txt`, 'utf8')
+
+const units = 'abcdefghijklmnopqrstuvwxyz';
+
+reactedPolymer = react(input);
+
+console.log(reactedPolymer.length); // 9900
+
+let shortestPolymerLength = reactedPolymer.length;
+
+for (let i = 0; i < units.length; i++){
+    const unitToDelete = units[i];
+    const filteredPolymer = removeUnits(unitToDelete, input);
+    const filteredReactedPolymer = react(filteredPolymer);
+    if (filteredReactedPolymer.length < shortestPolymerLength){
+        shortestPolymerLength = filteredReactedPolymer.length;
+    }
+}
+
+console.log(shortestPolymerLength); //
